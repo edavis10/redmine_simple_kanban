f45ab090be3aaf42fc8597086855fa8d6ca03078
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-19 14:41:48 -0700

    [#3910] Updated the init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,12 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_simple_kanban do
   name 'Redmine Simple Kanban plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Eric Davis'
+  description 'Simple implementation of a Kanban Board for use with Redmine to track tasks.'
+  url 'https://projects.littlestreamsoftware.com/projects/simple-kanban'
+  author_url 'http://www.littlestreamsoftware.com'
+
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.9.0'
 end
