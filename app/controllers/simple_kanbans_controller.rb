fbe870cfc10dd110281872ff83c84f61938726b8
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-19 16:58:25 -0700

    Include the Issues associations in the finder.

--- app/controllers/simple_kanbans_controller.rb
+++ app/controllers/simple_kanbans_controller.rb
@@ -2,10 +2,10 @@ class SimpleKanbansController < ApplicationController
   unloadable
 
   def show
-    @next_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('next_swimlane')})
-    @in_progress_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('in_progress_swimlane')})
-    @acceptance_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('acceptance_swimlane')})
-    @done_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('done_swimlane')})
+    @next_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('next_swimlane')}, :include => [:status, :priority, :tracker, :assigned_to])
+    @in_progress_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('in_progress_swimlane')}, :include => [:status, :priority, :tracker, :assigned_to])
+    @acceptance_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('acceptance_swimlane')}, :include => [:status, :priority, :tracker, :assigned_to])
+    @done_issues = Issue.visible.all(:conditions => {:status_id => status_id_for_swimlane('done_swimlane')}, :include => [:status, :priority, :tracker, :assigned_to])
     
   end
 
