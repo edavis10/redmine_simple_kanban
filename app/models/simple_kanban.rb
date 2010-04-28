50c27d96e7dc5e87beb99017e9cf4ab928b23e5c
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-28 09:56:40 -0700

    [#3886] Added a basic algorithm for finding the next issue.

---
+++ app/models/simple_kanban.rb
@@ -0,0 +1,21 @@
+class SimpleKanban
+  def self.next_issue
+    status_id = next_status_id
+    return nil if status_id.nil?
+    
+    Issue.visible.first(:conditions => {
+                          :status_id => status_id,
+                          :kanban_blocked => false
+                        },
+                        :order => 'due_date ASC, expedite DESC')
+  end
+
+  def self.next_status_id
+    if Setting.plugin_redmine_simple_kanban['next_swimlane'].present? &&
+        Setting.plugin_redmine_simple_kanban['next_swimlane']['status_id'].present?
+      Setting.plugin_redmine_simple_kanban['next_swimlane']['status_id']
+    else
+      nil
+    end
+  end
+end
---
+++ test/unit/simple_kanban_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+class SimpleKanbanTest < ActiveSupport::TestCase
+  context "SimpleKanban#next_issue" do
+    setup do
+      configure_plugin
+      User.current = @user = User.generate_with_protected!
+
+      @project = Project.generate!
+      @role = Role.generate!(:permissions => [:view_issues])
+      Member.generate!(:project => @project, :principal => @user, :roles => [@role])
+      
+      @dummy_issue = Issue.generate_for_project!(@project, :status => @in_progress_status)
+      @blocked_issue = Issue.generate_for_project!(@project, :status => @next_status, :kanban_blocked => true)
+      @in_progress_issue = Issue.generate_for_project!(@project, :status => @in_progress_status)
+      @accepted_issue = Issue.generate_for_project!(@project, :status => @acceptance_status)
+
+      @later_next_issue = Issue.generate_for_project!(@project, :subject => 'Due later', :status => @next_status, :due_date => 3.days.from_now)
+
+      @next_issue_non_expedite = Issue.generate_for_project!(@project, :subject => 'Not Expedite', :status => @next_status, :due_date => Date.tomorrow)
+      @next_issue = Issue.generate_for_project!(@project, :subject => 'Next issue', :status => @next_status, :due_date => Date.tomorrow, :expedite => true)
+      
+    end
+
+    should "find an issue that is visible" do
+      response = SimpleKanban.next_issue
+      assert response, "No issue returned"
+      assert response.visible?(@user), "Issue is not visible to the user."
+    end
+
+    should "find an issue in the Next swimlane" do
+      response = SimpleKanban.next_issue
+      assert response, "No issue returned"
+      assert_equal @next_status, response.status
+    end
+
+    should "not find an issue that is blocked" do
+      response = SimpleKanban.next_issue
+      assert response, "No issue returned"
+      assert !response.kanban_blocked?, "Returned a blocked issue."
+    end
+    
+    should "find the issue due soonest" do
+      response = SimpleKanban.next_issue
+      assert response, "No issue returned"
+      assert_equal @next_issue, response
+    end
+    
+    should "favor an expedited issue over a normal issue" do
+      response = SimpleKanban.next_issue
+      assert response, "No issue returned"
+      assert_equal @next_issue, response
+    end
+
+    context "with skill requirements" do
+      should "match skill requirements to a user"
+    end
+  end
+end
+  
