c64611d9bd0871176b7fd710293a32605aa72bf2
Eric Davis <edavis@littlestreamsoftware.com>
2012-09-04 16:09:52 -0700

    Prototype: show the total number of issues in each swimlane

--- app/helpers/simple_kanbans_helper.rb
+++ app/helpers/simple_kanbans_helper.rb
@@ -6,4 +6,12 @@ module SimpleKanbansHelper
       content_tag(:h2, l(:simple_kanban_text_next_issue))
     end
   end
+
+  def swimlane_label(content, grouped_count_of_issues)
+    label = l(content)
+    count = grouped_count_of_issues.inject(0) do |counter, key_value_pair|
+      counter += key_value_pair[1].length
+    end
+    content_tag(:h3, "#{label} (#{count})")
+  end
 end
